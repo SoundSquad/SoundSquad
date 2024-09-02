6de3ac9c82ccce08967438ec62c7adb0e4d53e86
anemoa <anemoia.yves@gmail.com>
2024-09-02 15:57:43 +0900

    Add: library utils.jsx file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        move: "move 5s linear infinite",
+      },
+      keyframes: {
+        move: {
+          "0%": { transform: "translateX(-200px)" },
+          "100%": { transform: "translateX(200px)" },
+        },
+      },
+    },
   },
   plugins: [],
 });
