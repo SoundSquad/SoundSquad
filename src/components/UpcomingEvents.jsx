a19ccf8ac9b21d15fb3e8ef7feefdd4a45a98355
pouryourlove <lqu134@naver.com>
2024-09-03 23:26:52 +0900

    feat: profile and genre

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -44,9 +44,7 @@ function UpcomingEvents() {
   // const artist = artists[0];
   return (
     <div className="max-w-5xl mx-auto">
-      <div className="mt-8  ">
-        <h3 className="text-xl font-bold mb-4">Upcoming events</h3>
-      </div>
+     
       <div>
         <Carousel
           items={cardsData.map((card, index) => (
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,25 @@
-import InfiniteMovingCards from "../components/InfiniteMovingCards"
+import GenreBtn from "../components/GenreBtn";
+import InfiniteMovingCards from "../components/InfiniteMovingCards";
+import UpcomingEvents from "../components/UpcomingEvents";
+import { CardDefault } from "../components/CardDefault";
 
 function MainPage() {
-    return (
-      <div className="flex justify-center">
-        <div className="w-full max-w-4xl">
-          <InfiniteMovingCards />
+  return (
+    <div className="flex justify-center">
+      <div className="w-full max-w-4xl">
+        <InfiniteMovingCards />
+        <h3 className="text-xl font-bold mb-4 mt-4">Profile</h3>
+        <UpcomingEvents />
+        <GenreBtn />
+        <div className="mt-24 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-11">
+          <CardDefault />
+          <CardDefault />
+          <CardDefault />
+          <CardDefault />
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default MainPage
+export default MainPage;
